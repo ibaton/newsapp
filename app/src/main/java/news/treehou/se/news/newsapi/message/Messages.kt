1a74724caa9786c9318ee3a2e0b756041f147fc4
ibaton <mattias.markehed@gmail.com>
2018-03-18 12:04:21 +0100

    Added comments to messages

--- app/src/main/java/news/treehou/se/news/newsapi/message/Messages.kt
+++ app/src/main/java/news/treehou/se/news/newsapi/message/Messages.kt
@@ -3,6 +3,9 @@ package news.treehou.se.news.newsapi.message
 import news.treehou.se.news.model.NewsArticle
 import news.treehou.se.news.model.NewsSource
 
+/**
+ * Messages that maps to the messages received from news api
+ */
 object Message {
     data class SourceMessage(val status: String, val sources: List<NewsSource> = listOf())
     data class ArticleMessage(val status: String, val articles: List<NewsArticle> = listOf())
