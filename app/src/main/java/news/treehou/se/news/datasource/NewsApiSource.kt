cb9f0b5ce86cb3c34463dd79103723c3a1995a86
ibaton <mattias.markehed@gmail.com>
2018-03-18 17:42:03 +0100

    Added comments to news api source.

--- app/src/main/java/news/treehou/se/news/datasource/NewsApiSource.kt
+++ app/src/main/java/news/treehou/se/news/datasource/NewsApiSource.kt
@@ -36,10 +36,20 @@ class NewsApiSource @Inject constructor() {
         return getSources().filterWatchedSources()
     }
 
+    /**
+     * Save data sources to database.
+     */
     fun updateSources(vararg source: NewsSource) {
         database.sourceDao().updateAll(*source)
     }
 
+    /**
+     * Search for articles and return a stream object emitting an object when articles are
+     * collected.
+     *
+     * @param searchText text to search for
+     * @return flowable emitting list of articles matching search term
+     */
     fun getArticles(searchText: String): Flowable<List<NewsArticle>> {
         return getSources().filterWatchedSources()
                 .filter { it.isNotEmpty() }
@@ -47,6 +57,11 @@ class NewsApiSource @Inject constructor() {
                 .onErrorReturn { listOf() }
     }
 
+    /**
+     * Get top headlines from watched sources.
+     *
+     * @return flowable emitting list of top headlines
+     */
     fun getTopHeadlines(): Flowable<List<NewsArticle>> {
         return getSources().filterWatchedSources()
                 .switchMap { getTopHeadlinesForSource(*it.toTypedArray()) }
@@ -86,6 +101,8 @@ class NewsApiSource @Inject constructor() {
 
     /**
      * Filter out sources that isn't watched.
+     *
+     * @return flowable emitting watch news sources.
      */
     fun Flowable<List<NewsSource>>.filterWatchedSources(): Flowable<List<NewsSource>> {
         return this.map { it.filter { source -> source.watched } }
